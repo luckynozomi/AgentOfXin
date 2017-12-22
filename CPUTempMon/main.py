cf62fd3d9f242c7188ed1c620658ba72d443f46f
suixin661014 <suixin661014@gmail.com>
2017-12-21 19:19:37 -0500

    initial commit

---
+++ CPUTempMon/main.py
@@ -0,0 +1,9 @@
+"""
+
+Monitor CPU Temperature of the Raspberry Pi 3, using command vcgencmd measure_temp
+
+Plot Temp vs. Time daily, upload it to Twitter
+
+When Temp > 80C, tweet the current temp and warn @nozomicomfy
+
+"""
