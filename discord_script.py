5c388149d9c6f7cde3922503bbca1e0feffe2835
suixin661014 <suixin661014@gmail.com>
2018-01-04 20:29:50 -0500

    initial commit

--- discord_script.py
+++ discord_script.py
@@ -4,14 +4,13 @@ import subprocess
 import logging
 
 from WeatherForecast.weather import *
-from WeatherForecast.tweet_alert import *
 
 # set up discord
 client = commands.Bot(command_prefix='!')
 
 # set up logging
 logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename=DIR_PATH + '/discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
@@ -28,40 +27,6 @@ async def print_discord(string, channel='general'):
     dest_channel = get_channel(client, channel)
     await client.send_message(dest_channel, string)
     return 0
-
-
-async def check_weather_daily():
-    """
-    Everyday at 7 A.M., fetch weather forecast for the day and report it to discord server.
-    Then report alerts, if there is any (defined in ParseForecast.report_alert() in WeatherForecast/weather.py), to
-    twitter.
-    :return: None
-    """
-
-    await client.wait_until_ready()
-
-    curr_time_and_zip = TimeAndZip()
-
-    if curr_time_and_zip.datetime.hour < 7:
-        next_time_and_zip = curr_time_and_zip.day_lapse(day_delta=0)
-    else:
-        next_time_and_zip = curr_time_and_zip.day_lapse(day_delta=1)
-
-    while True:
-
-        timedelta = next_time_and_zip.datetime - curr_time_and_zip.datetime
-        await asyncio.sleep(timedelta.total_seconds())
-
-        curr_time_and_zip = next_time_and_zip
-
-        xml = await curr_time_and_zip.fetch_forecast()
-        forecast = ParseForecast(xml=xml)
-        await forecast.report(zip_code=curr_time_and_zip.zip_code, date=curr_time_and_zip.datetime.date().isoformat(),
-                              func=print_discord)
-        await forecast.report_alert(zip_code=curr_time_and_zip.zip_code,
-                                    date=curr_time_and_zip.datetime.date().isoformat(),
-                                    func=twitter_update_status)
-        next_time_and_zip = curr_time_and_zip.day_lapse(day_delta=1)
 
 
 @client.event
@@ -112,5 +77,4 @@ async def weather(*args):
     forecast = ParseForecast(xml=xml)
     await forecast.report(zip_code=args[0], date=time_and_zip.datetime.date().isoformat(), func=print_discord)
 
-client.loop.create_task(check_weather_daily())
 client.run(TOKEN)
---
+++ twitter_script.py
@@ -0,0 +1,43 @@
+"""
+This script is run everyday at 7 A.M. to check weather.
+"""
+
+import tweepy
+
+from twitter_token import *
+from WeatherForecast.weather import *
+
+
+async def twitter_update_status(status):
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth)
+    api.update_status(status=status)
+
+
+async def main(zip_code='32304', debug=False):
+    """
+    Everyday at 7 A.M., fetch weather forecast for the day and report it to discord server.
+    Then report alerts, if there is any (defined in ParseForecast.report_alert() in WeatherForecast/weather.py), to
+    twitter.
+    :return: None
+    """
+
+    if debug is False:
+        print_func = twitter_update_status
+    else:
+        print_func = myprint
+
+    curr_time_and_zip = TimeAndZip(datetime_=datetime.now(), zip_code=zip_code)
+    xml = await curr_time_and_zip.fetch_forecast()
+    forecast = ParseForecast(xml=xml)
+    await forecast.report_alert(zip_code=curr_time_and_zip.zip_code,
+                                date=curr_time_and_zip.datetime.date().isoformat(),
+                                func=print_func)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(zip_code='32304', debug=True))
